e6931cb0b61909df8056c83151e839b31a406e6f
mlmiller4 <mlmiller4@gmail.com>
2019-09-09 14:25:36 -0400

    Update marketsimcode.py

--- marketsimcode.py
+++ marketsimcode.py
@@ -1,30 +1,3 @@
-"""MC2-P1: Market simulator.  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		   	  			    		  		  		    	 		 		   		 		  
-Atlanta, Georgia 30332  		   	  			    		  		  		    	 		 		   		 		  
-All Rights Reserved  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Template code for CS 4646/7646  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Georgia Tech asserts copyright ownership of this template and all derivative  		   	  			    		  		  		    	 		 		   		 		  
-works, including solutions to the projects assigned in this course. Students  		   	  			    		  		  		    	 		 		   		 		  
-and other users of this template code are advised not to share it with others  		   	  			    		  		  		    	 		 		   		 		  
-or to make it available on publicly viewable websites including repositories  		   	  			    		  		  		    	 		 		   		 		  
-such as github and gitlab.  This copyright statement should not be removed  		   	  			    		  		  		    	 		 		   		 		  
-or edited.  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-We do grant permission to share solutions privately with non-students such  		   	  			    		  		  		    	 		 		   		 		  
-as potential employers. However, sharing with other current or future  		   	  			    		  		  		    	 		 		   		 		  
-students of CS 7646 is prohibited and subject to being investigated as a  		   	  			    		  		  		    	 		 		   		 		  
-GT honor code violation.  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
------do not edit anything above this line---  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Student Name: Matthew L. Miller
-GT User ID: mmiller319
-GT ID: 903056227
-"""  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
 import pandas as pd  		   	  			    		  		  		    	 		 		   		 		  
 import numpy as np  		   	  			    		  		  		    	 		 		   		 		  
 import datetime as dt  		   	  			    		  		  		    	 		 		   		 		  
@@ -195,5 +168,3 @@ def test_code():
 if __name__ == "__main__":  		   	  			    		  		  		    	 		 		   		 		  
     test_code()
 
-def author():
-    return 'mmiller319'
